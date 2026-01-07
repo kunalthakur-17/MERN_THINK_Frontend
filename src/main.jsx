f646bd79d1ad68e04c608bda98598d47af96111e
Kunal Thakur <141919988+kunalthakur-17@users.noreply.github.com>
2026-01-08 04:15:56 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { configureStore } from './Redux/store'
-import { BrowserRouter } from 'react-router-dom'
 
 const store = configureStore();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-  <App />
-</BrowserRouter>
+      <App />
     </Provider>
   </StrictMode>,
 )
